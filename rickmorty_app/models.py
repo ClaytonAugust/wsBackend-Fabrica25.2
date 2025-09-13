cbccdedf0ff578fbb98cfbd6df7b19bc3c216d25
Clayton Augusto Nascimento de Araújo <augustoclayton57@gmail.com>
2025-09-13 19:02:28 -0300

    adicionado Episodio no models

--- rickmorty_app/models.py
+++ rickmorty_app/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class Episodio(models.Model):
+    api_id = models.IntegerField(unique=True)
+    nome = models.CharField(max_length=200, blank=True, null=True)
+    data_exibicao = models.CharField(max_length=100, blank=True, null=True)
+    codigo = models.CharField(max_length=50, blank=True, null=True)
+    personagens = models.ManyToManyField('Personagem', related_name='episodios')
+
+    def __str__(self):
+        return f"{self.codigo} - {self.nome}" if self.codigo and self.nome else f"Episódio ID: {self.api_id}"
+
 class Personagem(models.Model):
     api_id = models.IntegerField(unique=True)
     nome = models.CharField(max_length=200)
@@ -10,5 +20,3 @@ class Personagem(models.Model):
 
     def __str__(self):
         return self.nome
-
-
