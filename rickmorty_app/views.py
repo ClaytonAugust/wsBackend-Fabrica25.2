5efbba326d12bb92dd643b1d27a64cfcfcc5ba97
Clayton Augusto Nascimento de Araújo <augustoclayton57@gmail.com>
2025-09-13 12:23:30 -0300

    Criado func de importar e listar em views

--- rickmorty_app/views.py
+++ rickmorty_app/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import requests
+from django.shortcuts import render, redirect
+from .models import Personagem
 
-# Create your views here.
+# func para importar personagens da API
+def importar_personagens(request):
+    url = "https://rickandmortyapi.com/api/character"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        personagens_dados = response.json()
+        for item in personagens_dados['results']:
+            # cria ou atualiza o personagem no banco de dados
+            Personagem.objects.update_or_create(
+                api_id=item['id'],
+                defaults={
+                    'nome': item['name'],
+                    'status': item['status'],
+                    'especie': item['species'],
+                    'genero': item['gender'],
+                    'imagem': item['image']
+                }
+            )
+
+    return redirect('listar_personagens')
+
+# listagem de personagens
+def listar_personagens(request):
+    personagens = Personagem.objects.all()
+    return render(request, 'listar_personagens.html', {'personagens': personagens})
