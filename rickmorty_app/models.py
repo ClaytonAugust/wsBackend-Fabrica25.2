fa72b53d974cc80171fc05e2e407f3f04f7a0f14
Clayton Augusto Nascimento de Araújo <augustoclayton57@gmail.com>
2025-09-13 02:11:41 -0300

    :card_file_box: adicionado models e project url

--- rickmorty_app/models.py
+++ rickmorty_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Personagem(models.Model):
+    nome = models.CharField(max_length=200)
+    status = models.CharField(max_length=50)
+    especie = models.CharField(max_length=100)
+    genero = models.CharField(max_length=50)
+    imagem = models.URLField()
+
+    def __str__(self):
+        return self.nome
+
+class Episodio(models.Model):
+    titulo = models.CharField(max_length=200)
+    data_exibicao = models.DateField()
+    personagens = models.ManyToManyField(Personagem)
+
+    def __str__(self):
+        return self.titulo
+
