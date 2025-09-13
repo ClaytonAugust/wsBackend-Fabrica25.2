8732a1de48b5e2fab1466c22c1b1c377683b9e7c
Clayton Augusto Nascimento de Araújo <augustoclayton57@gmail.com>
2025-09-13 19:09:33 -0300

    adicionando novos paths

--- rickmorty_app/urls.py
+++ rickmorty_app/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('personagens/', views.listar_personagens, name='listar_personagens'),
-    path('importar/', views.importar_personagens, name='importar_personagens'),
+    path('episodios/', views.listar_episodios, name='listar_episodios'),
+    path('importar/personagens/otimizado/', views.importar_personagens_otimizado,
+         name='importar_personagens_otimizado'),
+    
     path('personagens/novo/', views.criar_personagem, name='criar_personagem'),
     path('personagens/editar/<int:api_id>/', views.editar_personagem, name='editar_personagem'),
     path('personagens/deletar/<int:api_id>/', views.deletar_personagem, name='deletar_personagem'),
