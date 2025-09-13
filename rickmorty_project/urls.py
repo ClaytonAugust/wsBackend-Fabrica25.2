c03e6c55c6b0f97f9cf90bdb93a8344ed2f59608
Clayton Augusto Nascimento de Araújo <augustoclayton57@gmail.com>
2025-09-13 10:58:04 -0300

    :bug: arrumado erro do project urls

--- rickmorty_project/urls.py
+++ rickmorty_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.include('rickmorty_app.urls')),
+    path('', include('rickmorty_app.urls')),
 ]
